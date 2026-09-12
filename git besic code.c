f352d2bb1d4154fcc763060ccb45edcea2a692f7
fatima <homairapakhi70@gmail.com>
2026-09-12 19:47:26 +0600

    check code

---
+++ git besic code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n % 2 != 0)
+    {
+        printf("Odd number");
+    }
+    else
+    {
+        printf("Even number");
+    }
+
+    return 0;
+}
